1d4a7dfcd29a6d733607ffdbfa5821d20dca90f4
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2022-04-26 21:49:43 +0300

    update, styles, packages

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,43 @@
 import './App.css';
+import React from 'react';
+import { Trans, useTranslation } from "react-i18next";
+import i18n from "./i18n";
+import GithubIcon from './GithubIcon';
 
 function App() {
+  const {t} = useTranslation();
+  const changeLanguage = () => {
+    let lng = 'en';
+    if (i18n.language === 'en') {
+      lng = 'ru';
+    }
+    i18n.changeLanguage(lng).then();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="app">
+        <header className="app-header">
+          <div>Stats4wiki</div>
+          <div>
+
+            <span>┬┴┬┴┤ ͜ʖ ͡°) <button className="lang-button" aria-label="Switch language (en/ru)"
+                                       title="Switch language (en/ru)" onClick={() => changeLanguage()}>
+              🌎</button>├┬┴┬┴</span>
+          </div>
+        </header>
+        <main className="main-content">
+          <h2 className="main-headers">{t("aboutTitle")}</h2>
+          <Trans i18nKey="aboutText">
+            WIP!
+          </Trans>
+        </main>
+        <footer className="footer-block">
+          <div>
+            <GithubIcon />
+          </div>
+          <div>2019-2022 <span role="img" aria-label="cookieEmoji">🍪</span></div>
+        </footer>
+      </div>
   );
 }
 
---
+++ web/src/i18n.js
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import detector from 'i18next-browser-languagedetector';
+
+const resources = {
+    en: {
+        translation: {
+            aboutTitle: "What is this project about?",
+            aboutText: "WIP!",
+        }
+    },
+    ru: {
+        translation: {
+            aboutTitle: "О чём этот проект?",
+            aboutText: "WIP!",
+        }
+    }
+}
+
+i18n.use(detector).use(initReactI18next).init({
+    fallbackLng: 'en',
+    resources,
+})
+
+export default i18n;
