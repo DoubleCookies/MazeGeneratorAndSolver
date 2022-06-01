56086c5dfc430ad0b625f19aa8b34db7c678c5af
DoubleCookies <12849336+DoubleCookies@users.noreply.github.com>
2022-06-01 15:18:46 +0300

    i18n, packages, fix gh link

--- web/src/App.js
+++ web/src/App.js
@@ -1,44 +1,43 @@
 import './App.css';
 import React from 'react';
-import { Trans, useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import i18n from "./i18n";
 import GithubIcon from './GithubIcon';
 
 function App() {
-  const {t} = useTranslation();
-  const changeLanguage = () => {
-    let lng = 'en';
-    if (i18n.language === 'en') {
-      lng = 'ru';
+    const {t} = useTranslation();
+    const changeLanguage = () => {
+        let lng = 'en';
+        if (i18n.language === 'en') {
+            lng = 'ru';
+        }
+        i18n.changeLanguage(lng).then();
     }
-    i18n.changeLanguage(lng).then();
-  }
 
-  return (
-      <div className="app">
-        <header className="app-header">
-          <div>Stats4wiki</div>
-          <div>
-
+    return (
+        <div className="app">
+            <header className="app-header">
+                <div>Stats4wiki</div>
+                <div>
             <span>┬┴┬┴┤ ͜ʖ ͡°) <button className="lang-button" aria-label="Switch language (en/ru)"
                                        title="Switch language (en/ru)" onClick={() => changeLanguage()}>
               🌎</button>├┬┴┬┴</span>
-          </div>
-        </header>
-        <main className="main-content">
-          <h2 className="main-headers">{t("aboutTitle")}</h2>
-          <Trans i18nKey="aboutText">
-            WIP!
-          </Trans>
-        </main>
-        <footer className="footer-block">
-          <div>
-            <GithubIcon />
-          </div>
-          <div>2019-2022 <span role="img" aria-label="cookieEmoji">🍪</span></div>
-        </footer>
-      </div>
-  );
+                </div>
+            </header>
+            <main className="main-content">
+                <h2 className="main-headers">{t("aboutTitle")}</h2>
+                <div className="textBlocks">{t("aboutText")}</div>
+                <h2 className="main-headers">{t("generationTitle")}</h2>
+                <div className="textBlocks">{t("generationText")}</div>
+            </main>
+            <footer className="footer-block">
+                <div>
+                    <GithubIcon/>
+                </div>
+                <div>2019-2022 <span role="img" aria-label="cookieEmoji">🍪</span></div>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
